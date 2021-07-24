e6aa24795e392b016e0a7e7cec493cee4b690e33
Luis Eduardo <ledarteaga@gmail.com>
2021-07-23 22:42:40 -0400

    Agregue el icono con js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ window.addEventListener("load", () => {
     let locationPosition = document.querySelector('.location-position');
     let temperatureDegree = document.querySelector('.temperature-degree');
     let temperatureDescription = document.querySelector('.description');
+    const temperatureIcon = document.querySelector('.temperature-icon');
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
@@ -11,7 +12,7 @@ window.addEventListener("load", () => {
             long = position.coords.longitude;
             
             const proxy = 'https://cors-anywhere.herokuapp.com/';
-            const api = `${proxy}api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=78f9fa8d63f5890688b8e10f35d7751c`;
+            const api = `${proxy}api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&lang=sp&appid=78f9fa8d63f5890688b8e10f35d7751c`;
 
             fetch(api)
             .then(response => {return response.json()})
@@ -19,13 +20,20 @@ window.addEventListener("load", () => {
                 { console.log(data);
                   const {name} = data;
                   const {temp} = data.main;
-                  const description = data.weather[0].description;
+                  const {description} = data.weather[0];
+                  const {country} = data.sys;
+                  const {icon} = data.weather[0];
 
                   //Cambiando el DOM con los datos de la API
 
-                  locationPosition.textContent = name;
+                  locationPosition.textContent = `${name}, ${country}`;
                   temperatureDegree.textContent = `${temp} C°`;
                   temperatureDescription.textContent = description;
+                  temperatureIcon.innerHTML = `<img src="icons/${icon}.png">`;
+
+                  
+                  
+
                   
                 })
                 .catch(err => {

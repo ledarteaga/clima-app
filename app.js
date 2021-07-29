63a682fcc539fbc703d37c871dadb6454d616979
Luis Eduardo <ledarteaga@gmail.com>
2021-07-28 20:10:07 -0400

    Proxy para que funcione el Api

--- app.js
+++ app.js
@@ -6,6 +6,25 @@ window.addEventListener("load", () => {
     let temperatureDescription = document.querySelector('.description');
     const temperatureIcon = document.querySelector('.temperature-icon');
 
+    // PROXY PARA QUE FUNCIONE EL API
+    
+    (function() {
+    var cors_api_host = 'cors-anywhere.herokuapp.com';
+    var cors_api_url = 'https://' + cors_api_host + '/';
+    var slice = [].slice;
+    var origin = window.location.protocol + '//' + window.location.host;
+    var open = XMLHttpRequest.prototype.open;
+    XMLHttpRequest.prototype.open = function() {
+        var args = slice.call(arguments);
+        var targetOrigin = /^https?:\/\/([^\/]+)/i.exec(args[1]);
+        if (targetOrigin && targetOrigin[0].toLowerCase() !== origin &&
+            targetOrigin[1] !== cors_api_host) {
+            args[1] = cors_api_url + args[1];
+        }
+        return open.apply(this, args);
+    };
+})();
+
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
             lat = position.coords.latitude;
